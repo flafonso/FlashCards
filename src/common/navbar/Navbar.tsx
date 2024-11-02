a163e3b021bf74098015323bbce401f2bcb92bc0
Flavien Afonso <af.flavien@gmail.com>
2024-11-02 18:46:18 +0100

    Updated Navbar component with login/profile links and added routes for login and profile pages

--- src/common/navbar/Navbar.tsx
+++ src/common/navbar/Navbar.tsx
@@ -1,21 +1,24 @@
-export function Navbar() {
-  return (
-    <nav className="px-6 py-1 mb-6 border-b-1 border-zinc-4 flex flex-justify-between flex-items-center bg-zinc-1 shadow-md
+import { Link } from "react-router-dom";
 
-">
+export function Navbar() {
+  const isLog: boolean = false;
+  return (
+    <nav className="px-6 py-1 mb-6 border-b-1 border-zinc-4 flex flex-justify-between flex-items-center bg-zinc-1 shadow-md">
       {/* logo */}
       <div className="i-gg-album text-6xl text-indigo-6"></div>
 
-
       <div>Découvrir</div>
 
-
       {/* login / profile */}
-      <div className="flex flex-items-center gap-1">
-        {/* <div className="i-gg-profile text-indigo"></div> */}
-        <div className="i-gg-log-in text-indigo-6 text-lg"></div>
-        Login
-      </div>
+      {isLog ? (
+        <Link to="/profile" className="flex flex-items-center gap-1">
+          <div className="i-gg-profile text-indigo-6 text-lg"></div> Profile
+        </Link>
+      ) : (
+        <Link to="/profile" className="flex flex-items-center gap-1">
+          <div className="i-gg-log-in text-indigo-6 text-lg"></div> Login
+        </Link>
+      )}
     </nav>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 // import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./app/App.tsx";
-import { HomePage } from "./pages";
+import { HomePage, LoginPage, ProfilePage } from "./pages";
 
 import "@unocss/reset/tailwind.css";
 import "virtual:uno.css";
@@ -16,6 +16,14 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <HomePage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "profile",
+        element: <ProfilePage />,
       },
     ],
   },
